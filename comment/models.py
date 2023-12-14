f65f787c6075fa5bdf65960c400fa42fc4409347
GentleOstrich <273819111@qq.com>
2023-12-14 23:53:49 +0800

    发表评论

--- comment/models.py
+++ comment/models.py
@@ -11,4 +11,4 @@ class Comment(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     time = models.CharField(max_length=20)  # 订票时间
     content = models.TextField(max_length=1000)
-    rating = models.IntegerField()
+    rating = models.IntegerField(default=0)
