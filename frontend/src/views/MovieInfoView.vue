d2109ad0c5982a237099a8ad316dac0c1548c082
GentleOstrich <273819111@qq.com>
2023-12-14 17:48:29 +0800

    commit

--- frontend/src/views/MovieInfoView.vue
+++ frontend/src/views/MovieInfoView.vue
@@ -4,6 +4,7 @@ import {ElMessage, ElMessageBox} from "element-plus";
 import {useRoute, useRouter} from "vue-router";
 import axios from "axios";
 import SideColomn from "../components/SideColomn.vue";
+import boolean from "async-validator/dist-types/validator/boolean";
 
 const route = useRoute()
 const router = useRouter()
@@ -16,6 +17,7 @@ const goBack = () => {
 
 interface Broadcast {
   id: BigInt;
+  hall_name: string;
   beginTime: string;
   endTime: string;
   seats: string;
@@ -49,7 +51,7 @@ const seat = ref(null);
 const buy = (index) => {
   // 新建影票并传给后端
   // 修改放映座位信息
-  if(aim_broadcast.value){
+  if (aim_broadcast.value) {
     axios
         .post(`/ticket/create/${aim_broadcast.value.id}/${index}/`)
         .then((res) => {
@@ -84,92 +86,139 @@ const form = ref({
   seats: ''
 })
 
-// const updateBroadcast = () => {
-//   const id = aim_broadcast.value.id
-//   const formData = new FormData();
-//   for (const key in form.value) {
-//     formData.append(key, form.value[key]);
-//   }
-//   axios
-//       .post(`/broadcast/update/${id}/`, formData)
-//       .then((res) => {
-//         if (res.data.errno === 0) {
-//           ElMessage.success('您已成功购票');
-//           router.go(0)
-//         } else {
-//           form.value.beginTime = ''
-//           form.value.endTime = ''
-//           form.value.seats = ''
-//         }
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//         ElMessage.error('系统错误');
-//       });
-// }
+const rating = ref(0);
+const content = ref("");
+const comments = ref([]);
 
+function getComments() {
+  axios.get("/api/comments")
+      .then((response) => {
+        // 获取评论数据
+        this.comments = response.data;
+      })
+      .catch((error) => {
+        // 获取评论数据失败
+      });
+}
+
+function onSubmit() {
+  // 获取评分和评论内容
+  const rating = rating.value;
+  const content = content.value;
+
+  // 发送评论数据
+  axios.post("/api/comments", {
+    rating,
+    content,
+  }).then((response) => {
+    // 评论提交成功
+  }).catch((error) => {
+    // 评论提交失败
+  });
+}
+
+function onRatingChange(value) {
+  this.rating = value;
+}
+
+function star() {
+
+}
+
+let isStarred = false;
 
 </script>
 
 <template>
 
-    <el-drawer v-model="ifshow" title="I am the title" :with-header="false">
-      <el-divider style="background: black">屏幕</el-divider>
-      <el-row justify="start">
-        <el-col
-            v-for="(i, index) in aim_broadcast.seats"
-            :span="6"
-        >
-          <div>
-            <el-button v-if="i == 0" style="background-color: navajowhite" @click="buy(index)"></el-button>
-            <el-button v-else style="background-color: orangered"
-                       @click="evt => ElMessageBox.alert('抱歉，此座位已售出')">
-            </el-button>
+  <el-drawer v-model="ifshow" title="I am the title" :with-header="false">
+    <el-divider style="background: black">屏幕</el-divider>
+    <el-row justify="start">
+      <el-col
+          v-for="(i, index) in aim_broadcast.seats"
+          :span="6"
+      >
+        <div>
+          <el-button v-if="i == 0" style="background-color: navajowhite" @click="buy(index)"></el-button>
+          <el-button v-else style="background-color: orangered"
+                     @click="evt => ElMessageBox.alert('抱歉，此座位已售出')">
+          </el-button>
 
-          </div>
+        </div>
+      </el-col>
+    </el-row>
+  </el-drawer>
+  <el-container>
+    <el-header>
+      <el-page-header @back="goBack" style="margin: 30px 0 0 20px;">
+
+      </el-page-header>
+    </el-header>
+
+    <el-main>
+      <el-image
+          :src=" 'data:image/jpeg;base64,' + movie_image"
+          class="image"
+          slot="error"
+          fit="cover"
+          style="max-width: 80%;max-height: 80%;width: auto;height: auto"
+      />
+      <el-header style="font-size: 60px; margin-bottom: 10px">{{ movie_name }}</el-header>
+      <!-- 收藏按钮 -->
+
+      <el-button circle size="mini">
+        收藏
+      </el-button>
+
+      <!-- 卡片风格 -->
+      <el-row justify="start" :gutter="30" style="margin-left: 50px;margin-right: 50px">
+        <el-col
+            v-for="broadcast in broadcasts"
+            :span="6"
+            style="flex: auto">
+          <el-card>
+            <el-text>
+              场馆名：{{ broadcast.hall_name }}
+            </el-text>
+            <el-text>
+              开始时间：{{ broadcast.beginTime }}
+            </el-text>
+            <el-text>
+              结束时间：{{ broadcast.endTime }}
+            </el-text>
+            <el-button v-if="!ifshow" style="margin-left: 100px" @click="ifshow=!ifshow, aim_broadcast=broadcast">
+              订票
+            </el-button>
+            <el-button v-else style="margin-left: 100px" @click="ifshow=!ifshow">
+              取消
+            </el-button>
+          </el-card>
         </el-col>
       </el-row>
-    </el-drawer>
-    <el-container>
-      <el-header>
-        <el-page-header @back="goBack" style="margin: 30px 0 0 20px;">
+    </el-main>
+  </el-container>
 
-        </el-page-header>
-      </el-header>
+  <div class="comment-container">
+    <el-rate
+        v-model="rating"
+        :max="5"
+        show-score
+        show-text
+        @change="onRatingChange"
+    />
+    <el-input
+        v-model="content"
+        placeholder="请输入评论内容"
+    />
+    <el-button type="primary" @click="onSubmit">提交评论</el-button>
 
-      <el-main>
-        <el-image
-            :src=" 'data:image/jpeg;base64,' + movie_image"
-            class="image"
-            slot="error"
-            style="max-width: 100%;max-height: 100%;width: auto;height: auto"
-        />
-        <el-header style="font-size: 60px">{{ movie_name }}</el-header>
-
-        <!-- 卡片风格 -->
-        <el-row justify="start" :gutter="30" style="margin-left: 50px;margin-right: 50px">
-          <el-col
-              v-for="broadcast in broadcasts"
-              :span="6"
-              style="flex: auto">
-            <el-card>
-              <el-text>
-                开始时间：{{ broadcast.beginTime }}
-              </el-text>
-              <el-text>
-                结束时间：{{ broadcast.endTime }}
-              </el-text>
-              <el-button v-if="!ifshow" style="margin-left: 100px" @click="ifshow=!ifshow, aim_broadcast=broadcast">
-                订票
-              </el-button>
-              <el-button v-else style="margin-left: 100px" @click="ifshow=!ifshow">
-                取消
-              </el-button>
-            </el-card>
-          </el-col>
-        </el-row>
-      </el-main>
-    </el-container>
+    <div class="comments">
+      <div class="comment" v-for="comment in comments" :key="comment.id">
+        <span class="rating">评分：{{ comment.rating }}</span>
+        <span class="content">{{ comment.content }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 
@@ -181,7 +230,7 @@ const form = ref({
 }
 
 .el-aside {
-  background-color: #fffff;
+  background-color: #ffffff;
   color: #333;
   text-align: center;
   line-height: 100px;
@@ -206,38 +255,8 @@ body > .el-container {
 .el-container:nth-child(7) .el-aside {
   line-height: 320px;
 }
+
+
 </style>
 
-<!--  <template>-->
-<!--  <el-container>-->
-<!--    <el-aside width="200px">-->
-<!--      {{ movie_name }}-->
-<!--    </el-aside>-->
-<!--    <el-container>-->
-<!--      <el-page-header @back="goBack" style="margin: 30px 0 0 20px;">-->
-<!--        <template #content>-->
-<!--          <span class="text-large font-600 mr-3"> 返回电影列表 </span>-->
-<!--        </template>-->
-<!--      </el-page-header>-->
-<!--      -->
-<!--      <el-main>-->
-<!--        <el-row justify="start" :gutter="30" style="margin-left: 50px;margin-right: 50px">-->
-<!--          <el-col-->
-<!--              v-for="test in tests"-->
-<!--              :span="6">-->
-<!--            <el-card>-->
-<!--              <el-text>-->
-<!--                {{ test }}-->
-<!--              </el-text>-->
-<!--              <router-link :to="{path:'/movies/index'}">-->
-<!--                <el-button style="margin-left: 100px">-->
-<!--                  订票-->
-<!--                </el-button>-->
-<!--              </router-link>-->
-<!--            </el-card>-->
-<!--          </el-col>-->
-<!--        </el-row>-->
-<!--      </el-main>-->
-<!--    </el-container>-->
-<!--  </el-container>-->
 
