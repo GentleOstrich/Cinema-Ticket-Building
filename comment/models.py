5519d2eaa561a1fd8ec3f56826cf1fe32abd10f9
GentleOstrich <273819111@qq.com>
2023-12-14 22:10:13 +0800

    commit

---
+++ comment/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from movie.models import Movie
+
+
+# Create your models here.
+class Comment(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # 订票用户
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    time = models.CharField(max_length=20)  # 订票时间
+    content = models.TextField(max_length=1000)
